6bac91b90b19c35ff1287f38de01c058bbd5c3b7
NER0 <NER0@outlook.com>
2016-11-13 12:06:28 +0800

    面试发布版本

--- src/interp/ConstChinese.java
+++ src/interp/ConstChinese.java
@@ -2,114 +2,112 @@ package interp;
 
 class ConstChinese {
 
-    public static final String NameOfPreviousValue = "*前者*";
+    static final String NameOfPreviousValue = "*前者*";
 
-    public static final String ConsolePrompt = "解释器";
+    static final String ConsolePrompt = "解释器";
 
-    public static final String SystemFinishedPrompt = "系统中止并输出";
+    static final String SystemFinishedPrompt = "系统中止并输出";
 
-    public static final String ErrorPrefix = "*** 系统错误: %s";
-    public static final String ErrorNotFoundSymbol = "未找到符号 %s";
-    public static final String ErrorNotFoundFile = "未找到文件 %s";
-    public static final String ErrorNotMatch = "模式不匹配";
-    public static final String ErrorSyntaxTooLittle = "输入代码太少";
-    public static final String ErrorSyntaxTooMuch = "输入代码太多";
-    public static final String ErrorSyntaxUndefined = "未定意常量 %s";
-    public static final String ErrorSyntaxIncorrectEscape = "非法转义序列";
-    public static final String ErrorCircleInsert = "列表不能插入到自身";
-    public static final String ErrorNameAlreadyExport = "符号已被导出了 %s";
-    public static final String ErrorArgsAmount = "输入参数数目不合 %s 在 %s";
-    public static final String ErrorType = "类型错误";
-    public static final String ErrorExternalSymbol = "禁止更改外部符号 %s";
+    static final String ErrorPrefix = "*** 系统错误: %s";
+    static final String ErrorNotFoundSymbol = "未找到符号 %s";
+    static final String ErrorNotFoundFile = "未找到文件 %s";
+    static final String ErrorNotMatch = "模式不匹配";
+    static final String ErrorSyntaxTooLittle = "输入代码太少";
+    static final String ErrorSyntaxTooMuch = "输入代码太多";
+    static final String ErrorSyntaxUndefined = "未定意常量 %s";
+    static final String ErrorSyntaxIncorrectEscape = "非法转义序列";
+    static final String ErrorCircleInsert = "列表不能插入到自身";
+    static final String ErrorNameAlreadyExist = "符号已被定意或导出了 %s";
+    static final String ErrorArgsAmount = "输入参数数目不合 %s 在 %s";
+    static final String ErrorType = "类型错误";
+    static final String ErrorExternalSymbol = "禁止更改外部符号 %s";
 
-    public static final String ImportNameSeparator = "";
+    static final String ImportNameSeparator = "";
 
-    public static final String ExprType = ":算式";
-    public static final String ListType = ":列表";
-    public static final String NumberType = ":数字";
-    public static final String StringType = ":文字";
-    public static final String BlobType = ":字节";
-    public static final String ExceptionType = ":异常";
-    public static final String HandleType = ":句柄";
-    public static final String NoneType = ":空";
-    public static final String SymbolType = ":符号";
-    public static final String TypeType = ":类型";
-    public static final String BoolType = ":真假";
-    public static final String LambdaType = ":算法";
+    static final String ExprType = ":算式";
+    static final String ListType = ":列表";
+    static final String NumberType = ":数字";
+    static final String StringType = ":文字";
+    static final String BlobType = ":字节";
+    static final String ExceptionType = ":异常";
+    static final String HandleType = ":句柄";
+    static final String NoneType = ":空";
+    static final String SymbolType = ":符号";
+    static final String TypeType = ":类型";
+    static final String BoolType = ":真假";
+    static final String LambdaType = ":算法";
 
-    public static final String Main = "入口";
-    public static final String MainArgs = "入口参数";
+    static final String Main = "入口";
+    static final String MainArgs = "入口参数";
 
-    public static final String ValueOfLambdaType = "组合算法";
-    public static final String ValueOfListType = "[]";
-    public static final String ValueOfExprType = "()";
-    public static final String ValueOfNoneType = "#空";
-    public static final String ValueOfBoolTypeTrue = "#真";
-    public static final String ValueOfBoolTypeFalse = "#假";
+    static final String ValueOfLambdaType = "组合算法";
+    static final String ValueOfListType = "[]";
+    static final String ValueOfExprType = "()";
+    static final String ValueOfNoneType = "#空";
+    static final String ValueOfBoolTypeTrue = "#真";
+    static final String ValueOfBoolTypeFalse = "#假";
 
-    public static final String LambdaToStringFormat = "(算法 %s %s)";
-    public static final String QuoteToStringFormat = "(解码 %s)";
+    static final String LambdaToStringFormat = "(算法 %s %s)";
 
+    static final String BoundArgs = "#参数";
+    static final String BoundLambda = "#算法";
 
-    public static final String BoundArgs = "#参数";
-    public static final String BoundLambda = "#算法";
+    static final String Define = "定意";
+    static final String Update = "更新";
 
-    public static final String Define = "定意";
-    public static final String Update = "更新";
+    static final String Cond = "选择";
+    static final String Eq = "等于";
+    static final String EqOp = "=";
+    static final String Lambda = "算法";
+    static final String Progn = "顺序";
+    static final String If = "如若";
+    static final String Apply = "施用";
+    static final String Quote = "引用";
+    static final String Let = "使";
+    static final String Match = "匹配";
+    static final String Import = "导入";
+    static final String Export = "导出";
+    static final String Eval = "求值";
+    static final String Type = "类型";
+    static final String Length = "长度";
+    static final String Assert = "断言";
+    static final String Exit = "退出";
 
-    public static final String Cond = "选择";
-    public static final String Eq = "等于";
-    public static final String EqOp = "=";
-    public static final String Lambda = "算法";
-    public static final String Progn = "顺序";
-    public static final String If = "如若";
-    public static final String Apply = "施用";
-    public static final String Quote = "引用";
-    public static final String Let = "使";
-    public static final String Match = "匹配";
-    public static final String Import = "导入";
-    public static final String Export = "导出";
-    public static final String Eval = "求值";
-    public static final String Type = "类型";
-    public static final String Length = "长度";
-    public static final String Assert = "断言";
-    public static final String Exit = "退出";
+    static final String Input = "输入";
+    static final String Output = "输出";
 
-    public static final String Input = "输入";
-    public static final String Output = "输出";
+    static final String Get = "取";
+    static final String Set = "改";
+    static final String Insert = "插入";
+    static final String Delete = "删除";
+    static final String Copy = "复制";
 
-    public static final String Get = "取";
-    public static final String Set = "改";
-    public static final String Insert = "插入";
-    public static final String Delete = "删除";
-    public static final String Copy = "复制";
+    static final String Substr = "片段";
+    static final String Concat = "拼接";
+    static final String Encode = "编码";
+    static final String Decode = "解码";
 
-    public static final String Substr = "片段";
-    public static final String Concat = "拼接";
-    public static final String Encode = "编码";
-    public static final String Decode = "解码";
+    static final String And = "与";
+    static final String Or = "或";
+    static final String Not = "非";
 
-    public static final String And = "与";
-    public static final String Or = "或";
-    public static final String Not = "非";
+    static final String Add = "加";
+    static final String AddOp = "+";
+    static final String Sub = "减";
+    static final String SubOp = "-";
+    static final String Mul = "乘";
+    static final String MulOp = "*";
+    static final String Div = "除";
+    static final String DivOp = "/";
+    static final String Mod = "余";
+    static final String ModOp = "%";
 
-    public static final String Add = "加";
-    public static final String AddOp = "+";
-    public static final String Sub = "减";
-    public static final String SubOp = "-";
-    public static final String Mul = "乘";
-    public static final String MulOp = "*";
-    public static final String Div = "除";
-    public static final String DivOp = "/";
-    public static final String Mod = "余";
-    public static final String ModOp = "%";
-
-    public static final String Gt = "大于";
-    public static final String GtOp = ">";
-    public static final String Ge = "大于等于";
-    public static final String GeOp = ">=";
-    public static final String Lt = "小于";
-    public static final String LtOp = "<";
-    public static final String Le = "小于等于";
-    public static final String LeOp = "<=";
+    static final String Gt = "大于";
+    static final String GtOp = ">";
+    static final String Ge = "大于等于";
+    static final String GeOp = ">=";
+    static final String Lt = "小于";
+    static final String LtOp = "<";
+    static final String Le = "小于等于";
+    static final String LeOp = "<=";
 }
--- src/interp/ConstEnglish.java
+++ src/interp/ConstEnglish.java
@@ -2,114 +2,112 @@ package interp;
 
 class ConstEnglish {
 
-    public static final String NameOfPreviousValue = "_";
+    static final String NameOfPreviousValue = "_";
 
-    public static final String ConsolePrompt = "REPL";
+    static final String ConsolePrompt = "REPL";
 
-    public static final String SystemFinishedPrompt = "System finished with";
+    static final String SystemFinishedPrompt = "System finished with";
 
-    public static final String ErrorPrefix = "*** System Error: %s";
-    public static final String ErrorNotFoundSymbol = "Not found symbol '%s'";
-    public static final String ErrorNotFoundFile = "Not found file '%s'";
-    public static final String ErrorNotMatch = "Match not match";
-    public static final String ErrorSyntaxTooLittle = "Too little input";
-    public static final String ErrorSyntaxTooMuch = "Too many input";
-    public static final String ErrorSyntaxUndefined = "Undefined literal '%s'";
-    public static final String ErrorSyntaxIncorrectEscape = "Incorrect escape";
-    public static final String ErrorCircleInsert = "Can not insert into one's itself";
-    public static final String ErrorNameAlreadyExport = "Already export, '%s'";
-    public static final String ErrorArgsAmount = "Arguments amount error, '%s' @ %s";
-    public static final String ErrorType = "Incorrect type";
-    public static final String ErrorExternalSymbol = "Forbidden to update external name '%s'";
+    static final String ErrorPrefix = "*** System Error: %s";
+    static final String ErrorNotFoundSymbol = "Not found symbol '%s'";
+    static final String ErrorNotFoundFile = "Not found file '%s'";
+    static final String ErrorNotMatch = "Match not match";
+    static final String ErrorSyntaxTooLittle = "Too little input";
+    static final String ErrorSyntaxTooMuch = "Too many input";
+    static final String ErrorSyntaxUndefined = "Undefined literal '%s'";
+    static final String ErrorSyntaxIncorrectEscape = "Incorrect escape";
+    static final String ErrorCircleInsert = "Can not insert into one's itself";
+    static final String ErrorNameAlreadyExist = "Name already exist '%s'";
+    static final String ErrorArgsAmount = "Arguments amount error, '%s' @ %s";
+    static final String ErrorType = "Incorrect type";
+    static final String ErrorExternalSymbol = "Forbidden to update external name '%s'";
 
-    public static final String ImportNameSeparator = "-";
+    static final String ImportNameSeparator = "-";
 
-    public static final String ExprType = ":expr";
-    public static final String ListType = ":list";
-    public static final String NumberType = ":number";
-    public static final String StringType = ":string";
-    public static final String BlobType = ":blob";
-    public static final String ExceptionType = ":exception";
-    public static final String HandleType = ":handle";
-    public static final String NoneType = ":none";
-    public static final String SymbolType = ":symbol";
-    public static final String TypeType = ":type";
-    public static final String BoolType = ":bool";
-    public static final String LambdaType = ":lambda";
+    static final String ExprType = ":expr";
+    static final String ListType = ":list";
+    static final String NumberType = ":number";
+    static final String StringType = ":string";
+    static final String BlobType = ":blob";
+    static final String ExceptionType = ":exception";
+    static final String HandleType = ":handle";
+    static final String NoneType = ":none";
+    static final String SymbolType = ":symbol";
+    static final String TypeType = ":type";
+    static final String BoolType = ":bool";
+    static final String LambdaType = ":lambda";
 
-    public static final String Main = "main";
-    public static final String MainArgs = "args";
+    static final String Main = "main";
+    static final String MainArgs = "args";
 
-    public static final String ValueOfLambdaType = "__lambda__";
-    public static final String ValueOfListType = "[]";
-    public static final String ValueOfExprType = "()";
-    public static final String ValueOfNoneType = "#none";
-    public static final String ValueOfBoolTypeTrue = "#true";
-    public static final String ValueOfBoolTypeFalse = "#false";
+    static final String ValueOfLambdaType = "__lambda__";
+    static final String ValueOfListType = "[]";
+    static final String ValueOfExprType = "()";
+    static final String ValueOfNoneType = "#none";
+    static final String ValueOfBoolTypeTrue = "#true";
+    static final String ValueOfBoolTypeFalse = "#false";
 
-    public static final String LambdaToStringFormat = "(lambda %s %s)";
-    public static final String QuoteToStringFormat = "(decode %s)";
+    static final String LambdaToStringFormat = "(lambda %s %s)";
 
+    static final String BoundArgs = "#args";
+    static final String BoundLambda = "#lambda";
 
-    public static final String BoundArgs = "#args";
-    public static final String BoundLambda = "#lambda";
+    static final String Define = "define";
+    static final String Update = "update";
 
-    public static final String Define = "define";
-    public static final String Update = "update";
+    static final String Cond = "cond";
+    static final String Eq = "eq";
+    static final String EqOp = "=";
+    static final String Lambda = "lambda";
+    static final String Progn = "progn";
+    static final String If = "if";
+    static final String Apply = "apply";
+    static final String Quote = "quote";
+    static final String Let = "let";
+    static final String Match = "match";
+    static final String Import = "import";
+    static final String Export = "export";
+    static final String Eval = "eval";
+    static final String Type = "type";
+    static final String Length = "length";
+    static final String Assert = "assert";
+    static final String Exit = "exit";
 
-    public static final String Cond = "cond";
-    public static final String Eq = "eq";
-    public static final String EqOp = "=";
-    public static final String Lambda = "lambda";
-    public static final String Progn = "progn";
-    public static final String If = "if";
-    public static final String Apply = "apply";
-    public static final String Quote = "quote";
-    public static final String Let = "let";
-    public static final String Match = "match";
-    public static final String Import = "import";
-    public static final String Export = "export";
-    public static final String Eval = "eval";
-    public static final String Type = "type";
-    public static final String Length = "length";
-    public static final String Assert = "assert";
-    public static final String Exit = "exit";
+    static final String Input = "input";
+    static final String Output = "output";
 
-    public static final String Input = "input";
-    public static final String Output = "output";
+    static final String Get = "get";
+    static final String Set = "set";
+    static final String Insert = "insert";
+    static final String Delete = "delete";
+    static final String Copy = "copy";
 
-    public static final String Get = "get";
-    public static final String Set = "set";
-    public static final String Insert = "insert";
-    public static final String Delete = "delete";
-    public static final String Copy = "copy";
+    static final String Substr = "substr";
+    static final String Concat = "concat";
+    static final String Encode = "encode";
+    static final String Decode = "decode";
 
-    public static final String Substr = "substr";
-    public static final String Concat = "concat";
-    public static final String Encode = "encode";
-    public static final String Decode = "decode";
+    static final String And = "and";
+    static final String Or = "or";
+    static final String Not = "not";
 
-    public static final String And = "and";
-    public static final String Or = "or";
-    public static final String Not = "not";
+    static final String Add = "add";
+    static final String AddOp = "+";
+    static final String Sub = "sub";
+    static final String SubOp = "-";
+    static final String Mul = "mul";
+    static final String MulOp = "*";
+    static final String Div = "div";
+    static final String DivOp = "/";
+    static final String Mod = "mod";
+    static final String ModOp = "%";
 
-    public static final String Add = "add";
-    public static final String AddOp = "+";
-    public static final String Sub = "sub";
-    public static final String SubOp = "-";
-    public static final String Mul = "mul";
-    public static final String MulOp = "*";
-    public static final String Div = "div";
-    public static final String DivOp = "/";
-    public static final String Mod = "mod";
-    public static final String ModOp = "%";
-
-    public static final String Gt = "gt";
-    public static final String GtOp = ">";
-    public static final String Ge = "ge";
-    public static final String GeOp = ">=";
-    public static final String Lt = "lt";
-    public static final String LtOp = "<";
-    public static final String Le = "le";
-    public static final String LeOp = "<=";
+    static final String Gt = "gt";
+    static final String GtOp = ">";
+    static final String Ge = "ge";
+    static final String GeOp = ">=";
+    static final String Lt = "lt";
+    static final String LtOp = "<";
+    static final String Le = "le";
+    static final String LeOp = "<=";
 }
